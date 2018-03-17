0501ae64614006809b6bd9152c2deb2fc95a67c3
Philippe Mulet <phmulet@Philippes-MacBook-Pro.local>
2018-03-17 01:26:04 -0700

    drop solution results

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -11,7 +11,7 @@ if [ -f $GRUNTFILE ]; then
   TEST_TYPE=fvt
 
   # publish results on solution
-  idra --publishtestresult --filelocation=$FILE_LOCATION --type=$TEST_TYPE
+  # idra --publishtestresult --filelocation=$FILE_LOCATION --type=$TEST_TYPE
 
   # publish results on all components
   source <(curl -sSL "https://raw.githubusercontent.com/open-toolchain/commons/master/scripts/publish_umbrella_test_results.sh.sh")
