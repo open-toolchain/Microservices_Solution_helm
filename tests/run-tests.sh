672f57f163ce5bcda4ec16c0db7d9cc98a9652aa
Philippe Mulet <hmagph@users.noreply.github.com>
2019-06-16 21:43:50 +0200

    convert to doi

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 GRUNTFILE="tests/Gruntfile.js"
 if [ -f $GRUNTFILE ]; then
-  export PATH=/opt/IBM/node-v4.2/bin:$PATH
   npm install
-  npm install -g grunt-idra3
+  npm install -g grunt-cli
+  
   set +e
   grunt test_real --gruntfile $GRUNTFILE --base .
   grunt_result=$?
@@ -11,11 +11,8 @@ if [ -f $GRUNTFILE ]; then
   FILE_LOCATION=./xunit.xml
   TEST_TYPE=fvt
 
-  # publish results on solution
-  # idra --publishtestresult --filelocation=$FILE_LOCATION --type=$TEST_TYPE
-
   # publish results on all components
-  source <(curl -sSL "https://raw.githubusercontent.com/open-toolchain/commons/master/scripts/publish_umbrella_test_results.sh")
+  source <(curl -sSL "https://raw.githubusercontent.com/open-toolchain/commons/hack/scripts/publish_umbrella_test_results.sh")
 
   if [ $grunt_result -ne 0 ]; then
      exit $grunt_result
